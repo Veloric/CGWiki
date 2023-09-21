b46378f751a898d0e5d044348ef851ebb5031b75
Kyle <krgamer444@gmail.com>
2023-09-20 23:43:23 -0400

    init

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 // Written by Veloric
 
 const express = require("express");
-const { google } = require("googleapis");
+const {google} = require("googleapis");
+const keys = require("/keys.json");
+
+
+//Begin inital set up of web app
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+
+app.listen(1337, (req, res) => console.log("Running on port 1337"));
